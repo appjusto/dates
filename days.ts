866f3399d13565155f96925e33d97da5a2a401b0
Pedro Andrade <pdandradeb@gmail.com>
2022-07-19 16:09:57 -0300

    Allowing getStartOfTheMonth and getStartOfTheWeek functions to be called without timezone

--- days.ts
+++ days.ts
@@ -9,19 +9,27 @@ export const getDayIndex = (date: Date | dayjs.Dayjs) => {
 
 export const getStartOfTheMonth = (
   date: Date = new Date(),
-  timezone: string = 'America/Sao_Paulo'
+  timezone?: string
 ) => {
-  const d = Dayjs(date).tz(timezone).date(1);
-  return Dayjs.tz(d.format('YYYY-MM-DD'), timezone).toDate();
+  if (timezone) {
+    const d = Dayjs(date).tz(timezone).date(1);
+    return Dayjs.tz(d.format('YYYY-MM-DD'), timezone).toDate();
+  } else {
+    return Dayjs(date).date(1).toDate();
+  }
 };
 
 export const getStartOfTheWeek = (
   date: Date = new Date(),
-  timezone: string = 'America/Sao_Paulo'
+  timezone?: string
 ) => {
-  const d = Dayjs(date).tz(timezone);
-  return Dayjs.tz(
-    d.subtract(getDayIndex(d), 'day').format('YYYY-MM-DD'),
-    timezone
-  ).toDate();
+  if (timezone) {
+    const d = Dayjs(date).tz(timezone);
+    return Dayjs.tz(
+      d.subtract(getDayIndex(d), 'day').format('YYYY-MM-DD'),
+      timezone
+    ).toDate();
+  } else {
+    return Dayjs(date).subtract(getDayIndex(date), 'day').toDate();
+  }
 };
