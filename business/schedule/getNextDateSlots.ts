8f36e368916e6fe17e13b4123fdf706852f3823e
Pedro Andrade <pdandradeb@gmail.com>
2022-07-26 10:19:48 -0300

    Adding limit to getNextDateSlots

--- business/schedule/getNextDateSlots.ts
+++ business/schedule/getNextDateSlots.ts
@@ -5,25 +5,35 @@ import { dateWithScheduleHour } from './hours';
 export const getNextDateSlots = (
   schedule: BusinessSchedule,
   date: Date,
-  interval: number = 30
-) =>
-  schedule.reduce<Date[][]>((result, { checked, schedule: daySchedule }, i) => {
-    if (!checked) return result;
-    const dates = daySchedule.reduce((r, { from, to }) => {
-      const f = Dayjs(
-        dateWithScheduleHour(date, from, 'America/Sao_Paulo')
-      ).add(i, 'day');
-      const t = Dayjs(dateWithScheduleHour(date, to, 'America/Sao_Paulo')).add(
-        i,
-        'day'
-      );
-      const r2: Date[] = [];
-      let n = f.clone().add(interval, 'minute');
-      while (n.isBefore(t)) {
-        if (n.diff(date, 'minute') > interval) r2.push(n.toDate());
-        n = n.clone().add(interval, 'minute');
-      }
-      return [...r, ...r2];
-    }, [] as Date[]);
-    return [...result, dates];
-  }, []);
+  interval: number = 30,
+  limit: number = Number.MAX_SAFE_INTEGER
+) => {
+  let total = 0;
+  return schedule.reduce<Date[][]>(
+    (result, { checked, schedule: daySchedule }, i) => {
+      if (!checked) return result;
+      const dates = daySchedule.reduce((r, { from, to }) => {
+        if (total >= limit) return r;
+        const f = Dayjs(
+          dateWithScheduleHour(date, from, 'America/Sao_Paulo')
+        ).add(i, 'day');
+        const t = Dayjs(
+          dateWithScheduleHour(date, to, 'America/Sao_Paulo')
+        ).add(i, 'day');
+        const r2: Date[] = [];
+        let n = f.clone().add(interval, 'minute');
+        while (n.isBefore(t)) {
+          if (total >= limit) break;
+          if (n.diff(date, 'minute') >= interval) {
+            total++;
+            r2.push(n.toDate());
+          }
+          n = n.clone().add(interval, 'minute');
+        }
+        return [...r, ...r2];
+      }, [] as Date[]);
+      return [...result, dates];
+    },
+    []
+  );
+};
